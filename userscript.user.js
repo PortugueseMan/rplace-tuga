ef5831ad1c4de3847bbe4964ecb605c4220801a8
PortugueseMan <109245070+PortugueseMan@users.noreply.github.com>
2023-07-20 23:12:56 +0100

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -9,39 +9,19 @@
 // @icon         https://i.pinimg.com/originals/bb/28/f0/bb28f08db88265208c7329474998d8a5.png
 // @grant        none
 // ==/UserScript==
+var overlayImage = null;
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
-            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
-        (function () {
-            const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/PortugueseMan/rplace-tuga/main/canva.png";
-            i.style = "position: absolute;left: 0;top: 0px;image-rendering: pixelated;width: 500px;height: 500px";
-            i.id = "mcss-overlay"
-            i.setAttribute("vis",1)
-            console.log(i);
-            return i;
-        })())
-
-            document.getElementsByTagName("body")[0].appendChild(
-        (function () {
-            const i = document.createElement("button");
-            i.style = "position: fixed;left: 10px; background: #fff; top: 50%;width: 50px;height: 50px;";
-            i.innerHTML = "T"
-            i.onclick = function() {
-                const img = document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.querySelector("#mcss-overlay")
-                if (img.getAttribute("vis") == "1") {
-                    img.setAttribute("vis",0)
-                    img.style.opacity  = 0
-                } else {
-
-                    img.setAttribute("vis",1)
-                    img.style.opacity  = 1
-                }
-            }
-            console.log(i);
-            return i;
-        })())
-
+        const canvasContainer = document.querySelector("garlic-bread-embed").shadowRoot.querySelector("div.layout").querySelector("garlic-bread-canvas").shadowRoot.querySelector("div.container");
+        overlayImage = document.createElement("img");
+        updateImage();
+        overlayImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;`;
+        canvasContainer.appendChild(overlayImage);
     }, false);
-
 }
+
+function updateImage() {
+    overlayImage.src = "https://github.com/PortugueseMan/rplace-tuga/blob/main/canva.png"
+}
+
+setInterval(function () {overlayImage.src = "https://github.com/PortugueseMan/rplace-tuga/blob/main/canva.png"}, 30000);
