87257390563a57c6baaf8f666bd4e1595950ca6b
Xhip <xhip@users.noreply.github.com>
2023-07-22 10:52:10 +0100

    Temporary fix offset position

--- userscript.user.js
+++ userscript.user.js
@@ -17,7 +17,7 @@ if (window.top !== window.self) {
         const canvasContainer = document.querySelector("garlic-bread-embed").shadowRoot.querySelector("div.layout").querySelector("garlic-bread-canvas").shadowRoot.querySelector("div.container");
         overlayImage = document.createElement("img");
         updateImage();
-        overlayImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1500px;height: 1000px;`;
+        overlayImage.style = `position: absolute;left: 0;top: 501px;image-rendering: pixelated;width: 1500px;height: 1000px;`;
         canvasContainer.appendChild(overlayImage);
     }, false);
 }
